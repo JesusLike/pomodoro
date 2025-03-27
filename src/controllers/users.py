faa145f0c859b5d0a49bb02f2ff5024385433c5c
dyudovich <yudovichml@gmail.com>
2025-03-27 17:22:23 +0200

    feature: authenticate user with credentials

--- src/controllers/users.py
+++ src/controllers/users.py
@@ -2,7 +2,7 @@
 from dataclasses import dataclass
 
 from src.database.users import UsersRepository
-from src.exceptions import UserNameAlreadyExists
+from src.exceptions import UserAlreadyExists
 from src.models.users import UserLoginCredentials
 
 from src.dependencies.security import hash_password
@@ -15,7 +15,7 @@ class UsersController():
         username = credentials.username
 
         if self.users_repository.get_user(username):
-            raise UserNameAlreadyExists()
+            raise UserAlreadyExists()
 
         hashed_password, salt = hash_password(credentials.password)
 
--- src/dependencies/security.py
+++ src/dependencies/security.py
@@ -1,6 +1,10 @@
 import bcrypt
 
-def hash_password(password: str) -> tuple[bytes, bytes]:
-        salt = bcrypt.gensalt()
-        hashed_password = bcrypt.hashpw(password.encode(), salt)
-        return (hashed_password, salt)
+def hash_password(password: str, salt: str | None = None) -> tuple[str, str]:
+    if not salt:
+        salt_bytes = bcrypt.gensalt()
+        salt = salt_bytes.decode()
+    else:
+        salt_bytes = salt.encode()
+    hashed_password: bytes = bcrypt.hashpw(password.encode(), salt_bytes)
+    return (hashed_password.decode(), salt)
